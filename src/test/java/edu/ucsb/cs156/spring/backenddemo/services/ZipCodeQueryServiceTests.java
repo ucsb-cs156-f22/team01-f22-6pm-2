64b4f0a23f5f201a771b1d11979b420a39e3bb44
crdgdr <q_li@ucsb.edu>
2022-10-13 18:16:21 -0700

    vl - started services

---
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/services/ZipCodeQueryServiceTests.java
@@ -0,0 +1,16 @@
+package edu.ucsb.cs156.spring.backenddemo.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
+
+public class ZipCodeQueryServiceTests {
+    
+}
