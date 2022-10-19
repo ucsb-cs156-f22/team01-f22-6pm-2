abbdac92c6137f97cca8e9cb48a011745351cf7c
Zackary Glazewski <zglazewski@umail.ucsb.edu>
2022-10-18 17:32:16 -0700

    ZG - remove uneccessary imports

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationControllerTests.java
@@ -45,7 +45,7 @@ public class LocationControllerTests {
     String location = "SantaMaria";
     when(mockLocationQueryService.getJSON(eq(location))).thenReturn(fakeJsonResult);
 
-    String url = String.format("/api/earthquakes/get?distance=%s",location);
+    String url = String.format("/api/location/get?location=%s",location);
 
     MvcResult response = mockMvc
         .perform( get(url).contentType("application/json"))
